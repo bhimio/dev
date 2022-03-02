5eb0a11527ef97b48166ee839c6d2893f9b00311
bhimio <bhimacassidy@gmail.com>
2022-03-02 12:43:46 -0500

    returning data

--- mod1/highRoler.java
+++ mod1/highRoler.java
@@ -14,17 +14,31 @@ public class highRoler {
 
         int dieSides = Integer.parseInt(stringDieSides);
 
+        System.out.println("how many dice do you have?");
+        String stringDieAmount = sc.nextLine();
+
+        int dieAmount = Integer.parseInt(stringDieAmount);
+
         Random diceRoller = new Random();
+        int whileLoop = 0;
+        int rollResult =  0;
+        do {
+            rollResult = diceRoller.nextInt(dieSides) + 1;
+            whileLoop ++;
+            if (dieAmount ==1) {
+                System.out.println("TIME TO ROOOOOOLL THE DICE!");
+            }
+            
+            System.out.println("You rolled a " + rollResult);
+            if (dieAmount == 1){
+                if (rollResult == 1) {
+                    System.out.println("You rolled a critical failure!");
+                } else if (rollResult == dieSides){
+                    System.out.println("you roled a critical sucses");
+                }
+            }
+        } while (dieAmount > whileLoop);
 
-        int rollResult = diceRoller.nextInt(dieSides) + 1;
-
-        System.out.println("TIME TO ROOOOOOLL THE DICE!");
-        System.out.println("I rolled a " + rollResult);
-
-        if (rollResult == 1) {
-            System.out.println("You rolled a critical failure!");
-        } else if (rollResult == dieSides){
-            System.out.println("you roled a critical sucsess");
-        }
+        
     }
 }
